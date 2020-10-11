517a710aa8a40fb9c26ef398b66b05f24ca136af
Jeevesh Jain <jain.39@iitj.ac.in>
2020-10-11 22:25:19 +0530

    test

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const getCurrentDate = () => {   //arrow function, can use function istead of =>
   return date.toDateString();
 }
 const greeting = <h1> Welcome to JeeDo:{getCurrentDate()}</h1>
-const dt = <h2></h2>
+//const dt = <h2>The best ever site</h2>
 
 
 
@@ -19,6 +19,7 @@ ReactDOM.render(
 greeting,
   document.getElementById('root')
 );
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
