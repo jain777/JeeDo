55c420320fb84c9aacd93917b8c1ed64aeac099b
Mohan Chhabaria <chhabaria.1@iitj.ac.in>
2020-10-16 20:09:59 +0530

    delete files

--- back-end/controllers/user.js
+++
@@ -1,8 +0,0 @@
-const jwt = require('jsonwebtoken');
-const authConfig = require('../configs/auth');
-const bcrypt = require('bcrypt');
-const sendmail = require('../utility/sendMail ');
-const database = require('../models/model');
-const auth = require('../configs/auth');
-const { text } = require('express');
-
